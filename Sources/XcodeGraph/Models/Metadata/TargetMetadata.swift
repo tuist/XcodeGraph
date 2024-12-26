850d3085bfb0e47f1fccfae75123eb70327d4bdf
Pedro <pedro@pepicrft.me>
2024-12-26 13:45:01 +0100

    fix: Turn isLocal into Target.type

--- Sources/XcodeGraph/Models/Metadata/TargetMetadata.swift
+++ Sources/XcodeGraph/Models/Metadata/TargetMetadata.swift
@@ -4,23 +4,15 @@ public struct TargetMetadata: Codable, Equatable, Sendable {
     /// Some Tuist features can leverage that information for doing things like filtering.
     public var tags: Set<String>
 
-    /// Projects can be external or not, which means they are declared or not using the Tuist Projects' DSL
-    /// and the targets of those projects can fall into two categories:
-    ///   - Local:  They've been linked from a local directory (e.g., a local package)
-    ///   - Remote: They've been resolved and pulled by a package manager (e.g. SPM)
-    public var isLocal: Bool
-
     @available(*, deprecated, renamed: "metadata(tags:)", message: "Use the static 'metadata' initializer instead")
     public init(
         tags: Set<String>
     ) {
         self.tags = tags
-        isLocal = true
     }
 
-    init(tags: Set<String>, isLocal: Bool) {
+    init(tags: Set<String>, isLocal _: Bool) {
         self.tags = tags
-        self.isLocal = isLocal
     }
 
     public static func metadata(tags: Set<String> = Set(), isLocal: Bool = true) -> TargetMetadata {
@@ -31,12 +23,10 @@ public struct TargetMetadata: Codable, Equatable, Sendable {
 #if DEBUG
     extension TargetMetadata {
         public static func test(
-            tags: Set<String> = [],
-            isLocal: Bool = true
+            tags: Set<String> = []
         ) -> TargetMetadata {
             TargetMetadata.metadata(
-                tags: tags,
-                isLocal: isLocal
+                tags: tags
             )
         }
     }
