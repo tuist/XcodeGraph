34fdd60cdc1e81a03d0932e4e1b847de56286621
Pedro <pedro@pepicrft.me>
2024-12-26 09:46:19 +0100

    Fix typo and run tests

--- Sources/XcodeGraph/Models/Metadata/TargetMetadata.swift
+++ Sources/XcodeGraph/Models/Metadata/TargetMetadata.swift
@@ -6,7 +6,7 @@ public struct TargetMetadata: Codable, Equatable, Sendable {
 
     /// Projects can be external or not, which means they are declared or not using the Tuist Projects' DSL
     /// and the targets of those projects can fall into two categories:
-    ///   - Local:  They've been linkd from a local directory (e.g., a local package)
+    ///   - Local:  They've been linked from a local directory (e.g., a local package)
     ///   - Remote: They've been resolved and pulled by a package manager (e.g. SPM)
     public var isLocal: Bool
 
