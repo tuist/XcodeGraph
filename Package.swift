5ce3dd516c16f53ee424eacc05adb1454905c550
Pedro <pedro@pepicrft.me>
2025-04-30 11:34:19 +0200

    Make the tests compile and pass

--- Package.swift
+++ Package.swift
@@ -84,7 +84,7 @@ let package = Package(
         .package(url: "https://github.com/tuist/Command.git", from: "0.13.0"),
         .package(url: "https://github.com/tuist/FileSystem.git", .upToNextMajor(from: "0.7.8")),
         .package(url: "https://github.com/apple/swift-service-context", .upToNextMajor(from: "1.2.0")),
-        .package(url: "https://github.com/Kolos65/Mockable.git", .upToNextMajor(from: "0.3.0")),
+        .package(url: "https://github.com/Kolos65/Mockable.git", .upToNextMajor(from: "0.3.1")),
         .package(url: "https://github.com/p-x9/MachOKit", .upToNextMajor(from: "0.30.0")),
         .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.4.3"),
     ],
