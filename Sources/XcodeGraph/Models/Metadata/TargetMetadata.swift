23a262c8ed6a5a58bab898ac4a147461e28cf944
Daniele Formichelli <df@bendingspoons.com>
2025-06-17 14:38:18 +0200

    feat: add TargetMetadata.ignoreRedundantDependencies

--- Sources/XcodeGraph/Models/Metadata/TargetMetadata.swift
+++ Sources/XcodeGraph/Models/Metadata/TargetMetadata.swift
@@ -4,29 +4,35 @@ public struct TargetMetadata: Codable, Equatable, Sendable {
     /// Some Tuist features can leverage that information for doing things like filtering.
     public var tags: Set<String>
 
+    /// Whether the target redundant dependencies should be ignored during `tuist inspect redundant-import`
+    public var ignoreRedundantDependencies: Bool
+
     @available(*, deprecated, renamed: "metadata(tags:)", message: "Use the static 'metadata' initializer instead")
     public init(
         tags: Set<String>
     ) {
-        self.tags = tags
+        self.init(tags: tags, isLocal: false, ignoreRedundantDependencies: false)
     }
 
-    init(tags: Set<String>, isLocal _: Bool) {
+    init(tags: Set<String>, isLocal _: Bool, ignoreRedundantDependencies: Bool) {
         self.tags = tags
+        self.ignoreRedundantDependencies = ignoreRedundantDependencies
     }
 
-    public static func metadata(tags: Set<String> = Set(), isLocal: Bool = true) -> TargetMetadata {
-        self.init(tags: tags, isLocal: isLocal)
+    public static func metadata(tags: Set<String> = Set(), isLocal: Bool = true, ignoreRedundantDependencies: Bool = false) -> TargetMetadata {
+        self.init(tags: tags, isLocal: isLocal, ignoreRedundantDependencies: ignoreRedundantDependencies)
     }
 }
 
 #if DEBUG
     extension TargetMetadata {
         public static func test(
-            tags: Set<String> = []
+            tags: Set<String> = [],
+            ignoreRedundantDependencies: Bool = false
         ) -> TargetMetadata {
             TargetMetadata.metadata(
-                tags: tags
+                tags: tags,
+                ignoreRedundantDependencies: ignoreRedundantDependencies
             )
         }
     }
