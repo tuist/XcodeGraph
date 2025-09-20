85e77585d688ea5fcebc129bff1cb44141d7d852
Pedro <pedro@pepicrft.me>
2025-09-20 11:10:00 +0200

    Make exceptions a sequence

--- Sources/XcodeGraph/Models/BuildableFolderExceptions.swift
+++ Sources/XcodeGraph/Models/BuildableFolderExceptions.swift
@@ -2,7 +2,7 @@ import Path
 
 /// PBXFileSystemSynchronizedRootGroup have a one-to-many relationship with PBXFileSystemSynchronizedBuildFileExceptionSet
 /// through .exceptions. Exceptions are used to exclude files and override conffigurations.
-public struct BuildableFolderExceptions: Sendable, Codable, Equatable, Hashable, ExpressibleByArrayLiteral {
+public struct BuildableFolderExceptions: Sendable, Codable, Equatable, Hashable, ExpressibleByArrayLiteral, Sequence {
     /// A list with all the exceptions.
     public var exceptions: [BuildableFolderException]
 
@@ -15,5 +15,10 @@ public struct BuildableFolderExceptions: Sendable, Codable, Equatable, Hashable,
 
     public init(exceptions: [BuildableFolderException]) {
         self.exceptions = exceptions
+    }
+
+    /// Returns an iterator over the contained exceptions.
+    public func makeIterator() -> [BuildableFolderException].Iterator {
+        exceptions.makeIterator()
     }
 }
